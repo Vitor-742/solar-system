2a503421e414d9d6494ac703a592aed4166635ab
Vitor Campos <vitorcampos742@gmail.com>
2022-01-03 18:08:50 -0300

    requisito 5

--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -4,11 +4,11 @@ import PropTypes from 'prop-types';
 class PlanetCard extends React.Component {
   render() {
     const { PlanetName, PlanetImage } = this.props;
-    const alt1 = 'Planeta', PlanetName
+    const alt1 = `Planeta ${PlanetName}`;
     return (
       <div data-testid="planet-card">
         <h4 data-testid="planet-name">{PlanetName}</h4>
-        <img src={ PlanetImage } alt= { alt1 } />
+        <img src={ PlanetImage } alt={ alt1 } />
       </div>
     );
   }
